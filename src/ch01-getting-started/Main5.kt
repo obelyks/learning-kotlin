b2804fff7cbee2ec347452d55792d7a2f4c641e0
Obelyks <obelyks@mail.com>
2019-02-09 13:44:19 +0100

    boring intro into creating class files

---
+++ src/ch01-getting-started/Main5.kt
@@ -0,0 +1,14 @@
+package my.program
+
+object App5 {
+    @JvmStatic fun main(args: Array<String>) {
+        println("Hello World")
+    }
+}
+
+// singleton?? nejpodobnejsi jave
+
+
+//classdir: learning-kotlin\out\production\learning-kotlin\..
+//\my\program
+//my.program.App5.class
